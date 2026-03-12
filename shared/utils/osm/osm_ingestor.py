27d23b057c57cee612012866bd05fad8cf46f834
markAndrewRay <146496186+markAndrewRay@users.noreply.github.com>
2026-03-12 21:16:02 +0000

    chore(refactor): update OSM function name to ingest_osm_features

--- shared/utils/osm/osm_ingestor.py
+++ shared/utils/osm/osm_ingestor.py
@@ -1,7 +1,7 @@
 import osmnx as ox
 import os
 
-def harvest_osm_features(place_name, tags, output_path):
+def ingest_osm_features(place_name, tags, output_path):
     gdf = ox.features_from_place(place_name, tags=tags)
     
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
